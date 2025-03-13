5d1e65a7e3a9ce74906ed9db336897246cbcc16a
pabloribeirodev <pablobronzeado@outlook.com>
2025-03-12 22:23:09 -0300

    criando metodo para carregar dados da api e metodo para cadastrar produto

--- src/app/form-produto-component/form-produto-component.component.ts
+++ src/app/form-produto-component/form-produto-component.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { ProdutoService } from './../servicos/produto.service';
 import { Produto } from './../model/Produto';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -14,10 +16,31 @@ export class FormProdutoComponentComponent {
 
   vetor:Produto[] = [];
 
- 
+  constructor(private servico:ProdutoService){}
+
   formulario = new FormGroup({
     id: new FormControl(null),
     nome: new FormControl(''),
     valor: new FormControl(null)
   })
+
+// Carregar os produtos assim que o componente for iniciado
+ngOnInit(): void {
+  this.carregarApi(); 
+}
+
+// Método para buscar os produtos na API
+carregarApi(): void {
+  this.servico.selecionar().subscribe(retorno => {
+    this.vetor = retorno; 
+  });
+}
+
+
+  cadastrar(){
+    this.servico.cadastrar(this.formulario.value as Produto).subscribe(retorno => {
+      this.vetor.push(retorno);
+      this.formulario.reset();
+    })
+  }
 }
